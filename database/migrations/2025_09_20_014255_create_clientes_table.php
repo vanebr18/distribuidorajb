e17ccb15c795256ddf8a3ca6b5e7d6534f0b8f1b
vanessabogado <vanessa.bogado@fctunca.edu.py>
2025-09-19 23:19:58 -0300

    feat(proveedores,clientes): agregar modelos y migraciones de proveedores y clientes

---
+++ database/migrations/2025_09_20_014255_create_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->boolean('estado')->default(true);
+            $table->integer('zona_id')->unsigned();
+            $table->foreign('zona_id')->references('id')->on('zonas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
